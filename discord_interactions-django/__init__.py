fb8795ae5b0db0b42bf1a62383027bea799bed72
catherineconsiglio <56280899+catherineconsiglio@users.noreply.github.com>
2022-05-01 18:23:55 -0400

    Flask wrapper converted to Django wrapper

--- discord_interactions-django/__init__.py
+++ discord_interactions-django/__init__.py
@@ -1,7 +1,6 @@
 __version__ = '0.3.0'
 
 from functools import wraps
-
 from nacl.exceptions import BadSignatureError
 from nacl.signing import VerifyKey
 
@@ -33,26 +32,21 @@ def verify_key(raw_body: bytes, signature: str, timestamp: str, client_public_ke
     return False
 
 def verify_key_decorator(client_public_key):
-    from flask import request, jsonify
-
-    # https://stackoverflow.com/questions/51691730/flask-middleware-for-specific-route
-    def _decorator(f):
-        @wraps(f)
-        def __decorator(*args, **kwargs):
-            # Verify request
-            signature = request.headers.get('X-Signature-Ed25519')
-            timestamp = request.headers.get('X-Signature-Timestamp')
-            if signature is None or timestamp is None or not verify_key(request.data, signature, timestamp, client_public_key):
+    import json
+    from django.http import HttpResponse
+    
+    def decorator(func):
+        @wraps(func)
+        def wrapper(request, *args, **kwargs):
+            signature = request.headers['X-Signature-Ed25519']
+            timestamp = request.headers['X-Signature-Timestamp']
+            jsonpayload = json.loads(request.body)
+            if signature is None or timestamp is None or not verify_key(request.body, signature, timestamp, client_public_key):
                 return 'Bad request signature', 401
-
-            # Automatically respond to pings
-            if request.json and request.json.get('type') == InteractionType.PING:
-                return jsonify({
+            if jsonpayload and jsonpayload['type'] == InteractionType.PING:
+                return HttpResponse(json.dumps({
                     'type': InteractionResponseType.PONG
-                })
-
-            # Pass through
-            return f(*args, **kwargs)
-        return __decorator
-    return _decorator
-
+                }))
+            return func(request, *args, **kwargs)
+        return wrapper
+    return decorator
